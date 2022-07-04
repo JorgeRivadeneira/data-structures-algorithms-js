59851fdf80be538d03da1f3983b97fa916984449
Jorge <jorge.rivadeneira.cevallos@gmail.com>
2022-07-04 17:36:07 -0500

    First Big O(n) notation

--- Big0_Demo1.js
+++ Big0_Demo1.js
@@ -1,11 +1,16 @@
-const nemo = ['nemo'];
-
+const all = ['cat', 'pill', 'sebastian', 'frankly', 'rivers', 'gonna', 'play', 'nemo'];
+const large = new Array(100000).fill('nemo');
 const findNemo = (array)=>{
+    let t0 = new Date();
     for(let i = 0; i < array.length; i++){
         if(array[i] === 'nemo'){
             console.log('Nemo has founded');
         }
     }
+    let t1 = new Date();
+    console.log('Finded in: ' +  (t1 - t0) + ' milliseconds');
+
 }
 
-findNemo(nemo);
+findNemo(large); // Our first O(n) notation, equal the numbers of elements and steps to find
+
